513a610abad3edaff955ea63d589ed48c9bd0e1f
Pat Maddox <pat@patmaddox.com>
2024-02-15 18:57:33 -0800

    a simple function to convert a google meeting to a tsml meeting

---
+++ a72_feed.rb
@@ -0,0 +1,28 @@
+require 'time'
+
+module A72Feed
+  def self.google_meeting_to_tsml(gm)
+    {
+      "name" => gm[0],
+      "slug" => gm[1],
+      "time" => time(gm[2]),
+      "location" => gm[11],
+      "notes" => gm[13],
+      "updated" => gm[18],
+      "types" => types(gm[14]),
+      "address" => gm[5],
+      "city" => gm[6],
+      "state" => gm[7],
+      "postal_code" => gm[8],
+      "country" => gm[9]
+    }
+  end
+
+  def self.time(s)
+    Time.parse(s).strftime("%H:%M")
+  end
+
+  def self.types(s)
+    s.split(",").map(&:strip)
+  end
+end
---
+++ test_a72_feed.rb
@@ -0,0 +1,48 @@
+require 'test/unit'
+require_relative 'a72_feed'
+
+class A72FeedTest < Test::Unit::TestCase
+  def test_google_meeting_to_tsml
+    google_meeting = [
+      name = "a wonderful meeting",
+      id = "meeting-id",
+      time = "8:00 AM",
+      day = "Sunday",
+      end_time = "",
+      address = "123 Main St",
+      city = "New York",
+      state = "NY",
+      postal_code = "10001",
+      country = "USA",
+      coordinates = "",
+      location = "a meeting hall",
+      district = "District 10",
+      notes = "this is my favorite meeting",
+      types = "O, TC, EN",
+      conference_url = "",
+      conference_phone = "",
+      venmo = "",
+      updated = "2024-01-02 00:00:00",
+      wwnotes = "",
+      wwname = "",
+      wwonlinenotes = ""
+    ]
+
+    tsml_meeting = A72Feed.google_meeting_to_tsml(google_meeting)
+
+    assert_equal tsml_meeting, {
+      "name" => name,
+      "slug" => id,
+      "time" => "08:00",
+      "location" => location,
+      "notes" => notes,
+      "updated" => updated,
+      "types" => ["O", "TC", "EN"],
+      "address" => address,
+      "city" => city,
+      "state" => state,
+      "postal_code" => postal_code,
+      "country" => country
+    }
+  end
+end
