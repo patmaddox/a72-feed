52acb37d6abd9463ca9a01785a96ff038c5e16cd
Pat Maddox <pat@patmaddox.com>
2024-10-18 19:24:01 -0700

    Exclude the header row

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@ require 'json'
 require 'open-uri'
 require_relative 'a72_feed'
 
-matching_values = ['district', 'District 10']
+district_index = 12
+district_name = 'District 10'
 
 ps_cso_url = ENV.fetch('PS_CSO_URL')
 data = URI.open(ps_cso_url).read
 json = JSON.parse(data)
-district_index = json['values'][0].index('district')
 meetings = json['values'].
-  select {|v| matching_values.include?(v[district_index]) }.
+  select {|v| v[district_index] == district_name }.
   map {|m| A72Feed.google_meeting_to_tsml(m) }
 puts JSON.dump(meetings)
